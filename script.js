eec54aa0e0b920503189ea92be2faaa18f2f43fe
CoderingMonkey <gamer_monkey@outlook.com.au>
2026-06-01 22:45:45 +1000

    cleanly styled the panels

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let rowWidth = window.innerWidth / 10
 let colNum = Math.floor(window.innerHeight / rowWidth)
 for (let i = 0; i < colNum; i++) {
     for (let j = 0; j < 10; j++) {
-        panelParent.createElement("div").textContent = `Row: ${j} / Col: ${i}`
+        panelParent.createElement("div").createElement("div").createElement("div").textContent = `Row: ${j} / Col: ${i}`
     }
 }
 panelParent.style.margin = `${(((window.innerHeight / rowWidth) - colNum) * rowWidth) / 2}px 0`
